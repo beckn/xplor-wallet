c1ce854d422caf4c109fd092eba3845908de0862
Bhaskar Kaura <bhaskar.kaura@thewitslab.com>
2024-04-23 20:59:41 +0530

    added crawler detection in view VC

--- src/verifiable-credential/service/verifiable-credential-read.service.ts
+++ src/verifiable-credential/service/verifiable-credential-read.service.ts
@@ -128,10 +128,14 @@ export class VerifiableCredentialReadService {
   This function returns the VC in pdf, image or the uploaded file format
    **/
   async renderVCDocument(restrictionKey: string, req, res): Promise<any> {
+    // Checking User Agent
+    const userAgent = req.headers['user-agent'] || ''
+    if (userAgent.includes('PageRenderer') || userAgent.includes('WhatsApp')) {
+      return
+    }
+
     // Fetch Access control details by restrictedKey
     // Finding Redis Cache to check if ACL Exists
-    const userAgent = req.headers['user-agent'] || ''
-    console.log('Request from a crawler:', userAgent)
     const aclDetails = await this.vcAclReadService.findCachedByRestrictedKey(restrictionKey)
     if (!aclDetails) {
       throw new NotFoundException(ViewAccessControlErrors.ACL_NOT_FOUND)
